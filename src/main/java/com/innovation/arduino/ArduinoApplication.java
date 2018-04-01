b3f83c7fdd4916c3f928f8c8e62aa75845b525a4
XZ-X <161250170@smail.nju.edu.cn>
2018-04-01 22:46:53 +0800

    no comment

--- src/main/java/com/innovation/arduino/ArduinoApplication.java
+++ src/main/java/com/innovation/arduino/ArduinoApplication.java
@@ -3,9 +3,7 @@ package com.innovation.arduino;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -14,10 +12,17 @@ public class ArduinoApplication {
 	public static void main(String[] args) throws IOException {
 //		SpringApplication.run(ArduinoApplication.class, args);
 		//"/bin/bash","-c","echo \"Fxsxwz1456\" | sudo -S
-		String cmd[]={"/bin/bash","-c","echo \" \"| sudo -S ls"};
-		Process process=Runtime.getRuntime().exec(cmd);
+//		String cmd[]={"/bin/bash","-c","echo \" \"| sudo -S /home/xxz/Documents/ardUART/run.sh /home/xxz/Documents/ardUART/test2.sh"};
+//		String cmd[]={"/bin/bash","-c","echo $TERM","ls","echo $TERM"};
+//		Process process=Runtime.getRuntime().exec(cmd);
+		Process process=Runtime.getRuntime().exec("/bin/bash",null,new File("/bin"));
 		Scanner scanner=new Scanner(new InputStreamReader(process.getInputStream()));
 		Scanner scanner1=new Scanner(new InputStreamReader(process.getErrorStream()));
+		PrintWriter writer=new PrintWriter(new OutputStreamWriter(process.getOutputStream()),true);
+		writer.println("export TERM=vt102");
+		writer.println("echo $TERM");
+		writer.println("/home/xxz/Documents/ardUART/tmp.sh");
+		writer.println("exit");
 		while(scanner.hasNext()){
 			System.out.println(scanner.next());
 		}
