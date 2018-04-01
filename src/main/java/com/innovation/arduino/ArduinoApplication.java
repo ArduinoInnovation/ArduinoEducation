fbbbc7441e39187cb5b7601b85e3c19ea95f79cc
XZ-X <161250170@smail.nju.edu.cn>
2018-04-01 15:32:06 +0800

    no message

--- src/main/java/com/innovation/arduino/ArduinoApplication.java
+++ src/main/java/com/innovation/arduino/ArduinoApplication.java
@@ -3,10 +3,30 @@ package com.innovation.arduino;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ArduinoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArduinoApplication.class, args);
+	public static void main(String[] args) throws IOException {
+//		SpringApplication.run(ArduinoApplication.class, args);
+		//"/bin/bash","-c","echo \"Fxsxwz1456\" | sudo -S
+		String cmd[]={"/bin/bash","-c","echo \" \"| sudo -S ls"};
+		Process process=Runtime.getRuntime().exec(cmd);
+		Scanner scanner=new Scanner(new InputStreamReader(process.getInputStream()));
+		Scanner scanner1=new Scanner(new InputStreamReader(process.getErrorStream()));
+		while(scanner.hasNext()){
+			System.out.println(scanner.next());
+		}
+		while (scanner1.hasNext()){
+			System.err.println(scanner1.next());
+		}
+
+
+
+
 	}
 }
