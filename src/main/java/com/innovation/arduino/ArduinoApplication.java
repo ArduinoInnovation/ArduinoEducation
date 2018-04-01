e8c86d03966ed7688bacd17810163457c2f1974a
161250170 <161250170@smail.nju.edu.cn>
2018-04-01 09:28:12 +0800

    first commit

---
+++ src/main/java/com/innovation/arduino/ArduinoApplication.java
@@ -0,0 +1,12 @@
+package com.innovation.arduino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArduinoApplication.class, args);
+	}
+}
